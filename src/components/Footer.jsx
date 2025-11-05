8ef3bc767a0f52f9edef4cdf1104b26c885a292c
Tolotra <rasamoelilala.tolotra@gmail.com>
2025-11-05 18:53:19 +0100

    footer mis a jour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,29 @@
 import '../assets/css/Footer.css'
-import githubIcon from '../assets/icons/github.png'
-import linkedinIcon from '../assets/icons/linkedIn.png'
-function Footer(){
-    return(
-        <section id="footer">
-            <div className="container d-flex">
-                <div className="d-flex gap-4">
-                <p><img src={githubIcon} className='img-fluid icon-footer' alt="icon github" /></p>
-                <p><img src={linkedinIcon} className='img-fluid icon-footer' alt="icon linkedIn" /></p>
-                </div>
-
-                <div>
-                    <p>
-                    Copyright © 2025 | Tous droits réservés
+import githubIcon from '../assets/img/icons/github-dark.svg';
+import linkedinIcon from '../assets/img/icons/linkedin-dark.svg';
+import dribbleIcon from '../assets/img/icons/dribble-dark.svg';
+function Footer() {
+    return (
+        <footer>
+            <div className="bg-footer">
+                <div className="container">
+                    <div className="footer-hr pb-2">
+                        <a href="https://github.com/Tolojanahary2001" target="_blank">
+                            <img src={githubIcon} width="50" alt="" />
+                        </a>
+                        <a href="https://mg.linkedin.com/in/tolojanahary-rasamoelilala-6b68b7331" target="_blank">
+                            <img src={linkedinIcon} width="50" alt="" />
+                        </a>
+                        <a href="https://dribbble.com/Tolotra_01" target="_blank">
+                            <img src={dribbleIcon} width="50" alt="" />
+                        </a>
+                    </div>
+                    <p className="text-center pt-2">
+                        Tolojanahary Rasamoelilala | Copyright © 2025 | Tous droits réservés
                     </p>
                 </div>
             </div>
-        </section>
+        </footer>
     )
 }
 export default Footer
