5ff16741f1a371f0b159a267a9d38781fb85225f
Tolotra <rasamoelilala.tolotra@gmail.com>
2025-11-06 20:03:19 +0100

    background animated mis a jour

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,24 +1,26 @@
 import '../assets/css/Background.css'
 
 function Background() {
-    <div className="bg-animated">
-        <div className="box">
-            <div className="square"></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+    return (
+        <div className="bg-animated">
+            <div className="box">
+                <div className="square"></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
-    </div>
+    );
 }
 export default Background;
